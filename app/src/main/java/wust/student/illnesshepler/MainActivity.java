267cec80b6c2d2a9ce47f6dc236bb62e67704e4f
LZA <1047771038@qq.com>
2019-09-26 23:10:48 +0800

    FirstCommit Test

---
+++ app/src/main/java/wust/student/illnesshepler/MainActivity.java
@@ -0,0 +1,14 @@
+package wust.student.illnesshepler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
