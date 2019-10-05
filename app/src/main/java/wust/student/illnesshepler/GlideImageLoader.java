cc411802241117fcc14bebe36abd232ac25d3104
aikebr <243344280@qq.com>
2019-10-05 11:56:48 +0800

    第一次上传

---
+++ app/src/main/java/wust/student/illnesshepler/GlideImageLoader.java
@@ -0,0 +1,17 @@
+package wust.student.illnesshepler;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+/*
+图片加载的第三依赖包
+ */
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        //Glide 加载图片简单用法
+        Glide.with(context).load(path).into(imageView);
+    }
+}
