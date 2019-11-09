18e1f97111ff95ffbe220424b9135b0825d79add
TenderLove <1047771038@qq.com>
2019-11-09 10:29:28 +0800

    优化推文详情页面滑动细节

--- app/src/main/java/wust/student/illnesshepler/Fragments/RepliesDetails.java
+++ app/src/main/java/wust/student/illnesshepler/Fragments/RepliesDetails.java
@@ -8,6 +8,7 @@ import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -21,12 +22,31 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import org.jetbrains.annotations.NotNull;
 
 import wust.student.illnesshepler.R;
-import wust.student.illnesshepler.Utils.StatusBarUtil;
+import wust.student.illnesshepler.Utils.ScreenUtil;
 
 public class RepliesDetails extends BottomSheetDialogFragment {
 
-    View view;
-    Toolbar toolbar;
+    private View view, nestScrollView;
+    private Toolbar toolbar;
+    private RelativeLayout relativeLayout;
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback
+            = new BottomSheetBehavior.BottomSheetCallback() {
+
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            //禁止拖拽，
+            if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                //设置为收缩状态
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
 
     // 创建View
     @Nullable
@@ -39,11 +59,40 @@ public class RepliesDetails extends BottomSheetDialogFragment {
 
     private void InitViews() {
         toolbar = view.findViewById(R.id.toolbar);
+        nestScrollView = view.findViewById(R.id.scrollViews);
         getActivity().setActionBar(toolbar);
         ActionBar actionbar = getActivity().getActionBar();
         if (actionbar != null) {
             actionbar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+
+        if (dialog != null) {
+            View bottomSheet = dialog.findViewById(R.id.design_bottom_sheet);
+            bottomSheet.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+        }
+        final View view = getView();
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                View parent = (View) view.getParent();
+                /*CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) (parent).getLayoutParams();
+                CoordinatorLayout.Behavior behavior = params.getBehavior();
+                bottomSheetBehavior = (BottomSheetBehavior) behavior;
+                bottomSheetBehavior.setBottomSheetCallback(mBottomSheetBehaviorCallback);
+                Display display = getActivity().getWindowManager().getDefaultDisplay();
+                //设置高度
+                int height = display.getHeight() / 4 * 3;
+                bottomSheetBehavior.setPeekHeight(height);*/
+
+                parent.setBackgroundColor(Color.TRANSPARENT);
+            }
+        });
     }
 
 
