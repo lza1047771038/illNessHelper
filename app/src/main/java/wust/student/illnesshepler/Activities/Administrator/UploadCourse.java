149a12f33df1b0bf73508eebf08cbc40ffbe7eea
aikebr <243344280@qq.com>
2019-12-08 16:22:42 +0800

    画了上传课程的布局

---
+++ app/src/main/java/wust/student/illnesshepler/Activities/Administrator/UploadCourse.java
@@ -0,0 +1,17 @@
+package wust.student.illnesshepler.Activities.Administrator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import wust.student.illnesshepler.R;
+
+public class UploadCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_course);
+
+    }
+}
